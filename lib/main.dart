808e9085c1a16445fc007335c2094e3f3c2f7ddc
Kareem Sobhi <kareemsobhi50@gmail.com>
2023-02-06 10:09:57 +0200

    Splash View

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bookly_app/features/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
+  }
+}
