1e53bf3fe4ada7cc14547c07fd7e60d66bc6f964
Kareem Sobhi <kareemsobhi50@gmail.com>
2023-02-10 00:36:17 +0200

    search view done

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/features/home/presentation/views/home_view.dart';
+import 'package:bookly_app/features/search/presentation/views/search_view.dart';
 import 'package:bookly_app/features/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
@@ -7,6 +8,7 @@ abstract class AppRouter
 {
   static const kHomeView = '/homeView';
   static const kBookDetailsView = '/bookDetailView';
+  static const kSearchView = '/searchView';
  static final router = GoRouter(
   routes: [
     GoRoute(
@@ -20,6 +22,10 @@ abstract class AppRouter
     GoRoute(
       path: kBookDetailsView,
       builder: (context, state) => const BookDetailView(),
+    ),
+    GoRoute(
+      path: kSearchView,
+      builder: (context, state) => const SearchView(),
     ),
   ],
 );
